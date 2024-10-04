97cce389046ce28528236a8561d4befb0a19908b
Julia Chebotarova <chebotarova.pn@ucu.edu.ua>
2024-10-04 11:44:12 +0300

    Add classes

---
+++ src/main/java/lotr/Knight.java
@@ -0,0 +1,9 @@
+package lotr;
+
+import java.util.Random;
+
+public class Knight extends Common{
+    public Knight() {
+        super(new Random().nextInt(11) + 2, new Random().nextInt(11) + 2);
+    }
+}
